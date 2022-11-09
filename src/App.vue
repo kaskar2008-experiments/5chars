2a1669be941df3fd270700431dc673253f129143
Kasparov Karen <dev.kaskar2008@gmail.com>
2022-11-09 19:34:46 +0300

    Initial-D

---
+++ src/App.vue
@@ -0,0 +1,182 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import GameLogo from './components/GameLogo.vue'
+import words from './words.json';
+
+const keyboard = [
+  ['й','ц','у','к','е','н','г','ш','щ','з','х','ъ'],
+  ['ф','ы','в','а','п','р','о','л','д','ж','э'],
+  ['я','ч','с','м','и','т','ь','б','ю']
+]
+
+function getRandomArbitrary(min: number, max: number) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const currentWord = ref('');
+const guesses = ref<any>([]);
+const myGuess = ref('');
+const canGuess = computed(() => guesses.value.length < 6);
+
+const addCharGuess = (char: string) => {
+  if (myGuess.value.length >= 5) {
+    return;
+  }
+
+  myGuess.value += char;
+}
+
+const deleteCharGuess = () => {
+  if (myGuess.value.length === 0) {
+    return;
+  }
+
+  myGuess.value = myGuess.value.substring(0, myGuess.value.length - 1);
+}
+
+currentWord.value = words[getRandomArbitrary(0, words.length - 1)];
+
+const doGuess = (window as any).doGuess = (word: string) => {
+  if (!canGuess.value || !word) {
+    return;
+  }
+
+  const newGuess = {
+    word,
+    info: [] as Array<string>,
+  };
+
+  word.toUpperCase().split('').forEach((char, index) => {
+    if (currentWord.value[index] === char) {
+      newGuess.info[index] = 'hit';
+      return;
+    }
+
+    if (!currentWord.value.includes(char)) {
+      newGuess.info[index] = 'wrong';
+      return;
+    }
+
+    if (currentWord.value.includes(char)) {
+      newGuess.info[index] = 'miss';
+      return;
+    }
+  });
+
+  guesses.value.push(newGuess);
+
+  myGuess.value = '';
+}
+
+const getCellClassName = (rindex: number, cindex: number) => {
+  const guess = guesses.value[rindex];
+
+  if (!guess) {
+    return '';
+  }
+
+  return guess.info[cindex];
+}
+</script>
+
+<template lang="pug">
+.app
+  header
+    game-logo(text="5chars")
+  main
+    .field
+      .row(v-for="(row, rindex) in 6")
+        .cell(v-for="(cell, cindex) in 5" :class="getCellClassName(rindex, cindex)")
+          span(v-if="guesses[rindex]") {{ guesses[rindex].word[cindex] }}
+      .row.my-guess
+        .cell(v-for="(cell, cindex) in 5")
+          span {{ myGuess[cindex] }}
+  footer
+    .keyboard(v-if="canGuess")
+      .row(v-for="(row, rindex) in keyboard")
+        template(v-if="rindex === 2")
+          .cell.ok(@click="doGuess(myGuess)") ✓
+        .cell(v-for="char in row" @click="addCharGuess(char)") {{ char }}
+        template(v-if="rindex === 2")
+          .cell.delete(@click="deleteCharGuess") ✕
+</template>
+
+<style scoped lang="stylus">
+.app
+  min-height 100vh
+  max-width 500px
+  width 100%
+  margin 0 auto
+  padding 50px 0
+  box-sizing border-box
+  display grid
+  grid-template-rows auto 1fr auto
+  grid-gap 24px
+
+header
+  padding 8px
+
+.field
+  padding 8px
+  display grid
+  grid-gap 10px
+
+  .row
+    display flex
+    gap 10px
+
+  .cell
+    width 25%
+    aspect-ratio 1 / 1
+    border 1px solid yellow
+    border-radius 8px
+    font-size 40px
+    line-height 1.6
+    display flex
+    align-content center
+    justify-content center
+    text-transform uppercase
+
+    &.wrong,
+    &.miss,
+    &.hit
+      border none
+
+    &.wrong
+      background-color #555
+
+    &.miss
+      background-color #fff
+      color black
+
+    &.hit
+      background-color yellow
+      color black
+
+    span
+      display block
+
+.keyboard
+  display flex
+  flex-direction column
+  gap 10px
+
+  .row
+    display flex
+    margin 0 auto
+    gap 3px
+
+  .cell
+    border 1px solid #444
+    padding 5px 9px
+    border-radius 5px
+
+    &.ok,
+    &.delete
+      border 1px solid rgba(0,0,0,0)
+
+    &.ok
+      background-color green
+    &.delete
+      background-color red
+</style>
---
+++ src/components/GameLogo.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+.logo-text
+  span(v-for="char in text.toUpperCase().split('')") {{ char }}
+</template>
+
+<script setup lang="ts">
+defineProps<{ text: string }>()
+</script>
+
+<style scoped lang="stylus">
+.logo-text
+  display inline-flex
+  gap 2px
+  color black
+  font-weight 600
+  font-size 20px
+
+  span
+    display flex
+    flex-grow 1
+    width 30px
+    height 30px
+    align-items center
+    background-color yellow
+    border-radius 5px
+    justify-content space-evenly
+</style>
