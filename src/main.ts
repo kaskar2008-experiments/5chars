d55fe9a4c7207716369e029e49d29810f3def659
Kasparov Karen <dev.kaskar2008@gmail.com>
2023-01-26 14:36:46 +0300

    feat: service-worker

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
+
+window.addEventListener("load", () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("service-worker.js");
+  }
+});
